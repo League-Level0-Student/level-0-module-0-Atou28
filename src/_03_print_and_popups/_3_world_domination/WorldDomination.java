a8b69fa6baae34cce15803dde1390936b6f1c11e
league <league@wts14-3.attlocal.net>
2023-03-03 16:56:48 -0800

    I completed 3 games again including a quiz!

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -8,9 +8,13 @@ public class WorldDomination {
 	public static void main(String[] args) {
 		
 		// 1. Ask the user if they know how to write code.
-
+		String code = JOptionPane.showInputDialog("Do you know how to write code? (yes/no)");
 		// 2. If they say "yes", tell them they will rule the world.
-
+		if(code.equalsIgnoreCase( "yes"  )) {
+			JOptionPane.showMessageDialog(null,"You will rule the world!!!");
+		} else {
+			JOptionPane.showMessageDialog(null,"Sign up for classes at the league!!");
+		}
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
 	}
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,23 +1,66 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
-		
+
 		// Create a variable to hold the user's score. Set it equal to zero. 
-		
+		int score = 0;
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
-				// 2.  Ask the user a question 
-				
-				// 3.  Use an if statement to check if their answer is correct
-				
-				// 4.  if the user's answer was correct, add one to their score 
-		
+
+		// 2.  Ask the user a question 
+		String answer = JOptionPane.showInputDialog("How old is Andrew Tou?");
+		// 3.  Use an if statement to check if their answer is correct
+		if(answer.equalsIgnoreCase( "11"  )) {
+			// 4.  if the user's answer was correct, add one to their score 
+			score += 1;
+		}   else {
+			score -= 1;
+		}
+
+		answer = JOptionPane.showInputDialog("what is 1 doubled (as in 1, 2, 4 ,8, etc.) 16");
+		// 3.  Use an if statement to check if their answer is correct
+		if(answer.equalsIgnoreCase( "65536"  ) || answer.equals("65,536")) {
+			// 4.  if the user's answer was correct, add one to their score 
+			score += 1;
+		}   else {
+			score -= 1;
+		}
+		answer = JOptionPane.showInputDialog("who is the second president?");
+		// 3.  Use an if statement to check if their answer is correct
+		if(answer.equalsIgnoreCase( "John Adams"  ) || answer.equalsIgnoreCase("Adams")) {
+			// 4.  if the user's answer was correct, add one to their score 
+			score += 1;
+		}   else {
+			score -= 1;
+		}
+		answer = JOptionPane.showInputDialog("who is the 99th Pokemon?");
+		// 3.  Use an if statement to check if their answer is correct
+		if(answer.equalsIgnoreCase( "Kingler"  )) {
+			// 4.  if the user's answer was correct, add one to their score 
+			score += 1;
+		}   else {
+			score -= 1;
+		}
+		answer = JOptionPane.showInputDialog("Kingler is the evolved form of which Pokemon?");
+		// 3.  Use an if statement to check if their answer is correct
+		if(answer.equalsIgnoreCase( "Krabby"  )) {
+			// 4.  if the user's answer was correct, add one to their score 
+			score += 1;
+		}   else {
+			score -= 1;
+		}
+
+
+
+
+
 		// MAKE MORE QUESTIONS. Ask more questions by repeating the above 
-				// Option: Subtract a point from their score for a wrong answer
-		
-		
+		// Option: Subtract a point from their score for a wrong answer
+
+
 		// After all the questions have been asked, tell the user their final score 
-		
+		JOptionPane.showMessageDialog(null,"your score is " + score);
 	}
 }
